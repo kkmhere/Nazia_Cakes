7f973b1e0ae59d3742ea9c0fcb82399dcbcf8595
kkmhere <kkm01.1998@gmail.com>
2019-06-23 16:06:57 +0530

    toolbar added

--- app/src/main/java/com/example/naziacakes/MainActivity.java
+++ app/src/main/java/com/example/naziacakes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.naziacakes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.mytoolbar);
+        if(toolbar!=null)
+        {
+            setSupportActionBar(toolbar);
+        }
+
+
     }
 }
