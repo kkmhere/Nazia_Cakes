02ba04d1e0d2995b6f71c477ebf92d547cbbd6df
kkmhere <kkm01.1998@gmail.com>
2019-06-23 17:02:30 +0530

    navigation added

--- app/src/main/java/com/example/naziacakes/MainActivity.java
+++ app/src/main/java/com/example/naziacakes/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.naziacakes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,43 @@ public class MainActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
+        final DrawerLayout drawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(MainActivity.this,drawerLayout,toolbar,R.string.open,R.string.close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Toast.makeText(MainActivity.this,"OPENED",Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                Toast.makeText(MainActivity.this,"CLOSED",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sidemenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.cart:
+                Toast.makeText(MainActivity.this,"CART OPENING",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.profile:
+                Toast.makeText(MainActivity.this,"PROFILE OPENING",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
